24f5a91a2069fadab7b39985bfadb3fa2adcca40
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2023-11-18 22:48:57 -0500

    Add Firebase Hosting & tools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMGYYNucmuPkOUXUD1jOelTFc5iWQd1QA',
+  authDomain: 'softballgametrackerui.firebaseapp.com',
+  projectId: 'softballgametrackerui',
+  storageBucket: 'softballgametrackerui.appspot.com',
+  messagingSenderId: '1054208583100',
+  appId: '1:1054208583100:web:9585350863a26727b9b7b9',
+  measurementId: 'G-SZQG8J379Y',
+};
 
 @Component({
   selector: 'app-root',
@@ -9,4 +21,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  // Initialize Firebase
+  app = initializeApp(firebaseConfig);
+  analytics = getAnalytics(this.app);
+}
