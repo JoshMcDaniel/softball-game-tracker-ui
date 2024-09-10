196d0e0a30da981439f43b5458e10c37dbf0364f
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2024-09-10 10:33:46 -0400

    Add style to Login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,47 +1,57 @@
 import { Component, inject, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/services/auth.service';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Subject, switchMap, takeUntil } from 'rxjs';
 import { User } from '@angular/fire/auth';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnDestroy {
   authService = inject(AuthService);
-  fb = inject(FormBuilder);
+  // fb = inject(FormBuilder);
   http = inject(HttpClient);
   router = inject(Router);
 
   private _unsubscribe = new Subject<void>();
 
-  form = this.fb.nonNullable.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required],
-  });
+  // form = this.fb.nonNullable.group({
+  //   email: ['', Validators.required],
+  //   password: ['', Validators.required],
+  // });
+
   errorMessage: string | null = null;
 
-  onSubmit(): void {
-    this.errorMessage = null;
-    const rawForm = this.form.getRawValue();
-    this.authService
-      .loginWithEmailAndPassword(rawForm.email, rawForm.password)
-      .pipe(
-        takeUntil(this._unsubscribe),
-        switchMap(() => this.authService.user$)
-      )
-      .subscribe({
-        next: (user) => this.handleSuccessfulLogin(user),
-        error: (err) => (this.errorMessage = err.code),
-      });
-  }
+  // onSubmit(): void {
+  //   this.errorMessage = null;
+  //   const rawForm = this.form.getRawValue();
+  //   this.authService
+  //     .loginWithEmailAndPassword(rawForm.email, rawForm.password)
+  //     .pipe(
+  //       takeUntil(this._unsubscribe),
+  //       switchMap(() => this.authService.user$)
+  //     )
+  //     .subscribe({
+  //       next: (user) => this.handleSuccessfulLogin(user),
+  //       error: (err) => (this.errorMessage = err.code),
+  //     });
+  // }
 
   loginWithGoogle(): void {
     this.errorMessage = null;
@@ -53,7 +63,7 @@ export class LoginComponent implements OnDestroy {
       )
       .subscribe({
         next: (user) => this.handleSuccessfulLogin(user),
-        error: (err) => (this.errorMessage = err.code),
+        error: () => this.handleUnsuccessfulLogin(),
       });
   }
 
@@ -61,6 +71,10 @@ export class LoginComponent implements OnDestroy {
     if (user) {
       this.router.navigateByUrl('/dashboard');
     }
+  }
+
+  handleUnsuccessfulLogin(): void {
+    this.errorMessage = 'Unable to login. Please try again later.';
   }
 
   ngOnDestroy(): void {
